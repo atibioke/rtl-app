82fb3dde8581d9b3eb55cada8016e8390e58aa90
Rasaq Ganiyu <eudoxusganium@gmail.com>
2023-11-16 18:56:19 +0100

    writing test for the application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import {nanoid} from 'nanoid';
 
 function App() {
+  const id = nanoid(10);
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = e => {
+    const textValue = e.target.value;
+    setText(textValue);
+  };
+
+  const handleSubmit = () => {
+    setTodos(todo => [...todo, {text, id}]);
+    setText('');
+  };
+
+  const handleRemoveTodo = value => {
+    const todoResult = todos.filter(todo => todo.id !== value);
+    setTodos(todoResult);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-container">
+        <div className="input-inner-container">
+          <input
+            type="text"
+            placeholder="Your Todo..."
+            value={text}
+            onChange={handleChange}
+          />
+          <button onClick={() => handleSubmit()}>Submit</button>
+        </div>
+        <div className="todo-container">
+          <h4 className="todo-header">Your Todos ({todos?.length})</h4>
+          <div className="todo-inner-container">
+            {todos?.map((todo, index) => (
+              <div
+                key={todo?.id}
+                className="todos"
+                onClick={() => handleRemoveTodo(todo?.id)}>
+                <div>{todo?.text}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
